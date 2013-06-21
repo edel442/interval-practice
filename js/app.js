0deb1bb31c7b2d2a20f1edf5cbf1a20829fdce29
edel <edel.lock@if-core.de>
2013-06-21 12:54:27 +0200

    first function added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 'use strict';
 
-var cbb = {
-	init: function() {
-		console.log('Hello World!');
-	}
+var init = function() {
+	console.log('Hello World!');
 };
+
+
+
+var intervals = ["Prime", "Sekunde", "kleine Terz", "große Terz", "Quarte", "Quinte", "Sexte", "Septime", "Oktave"];
+
+
+var userAnswer = prompt("Nenne das Intervall von c' nach c'") {
+	if (userAnswer === "Prime") {
+		console.log("Richtig, es ist die Prime");
+	}
+	else {
+		console.log ("Das ist leider nicht richtig. Versuchen Sie es noch einmal.");
+	}
+}
