bf1a8d60234bb20363dc40b8d9af3f621663d5c9
edel <edel.lock@if-core.de>
2013-07-10 14:19:02 +0200

    added another html-page

--- js/app.js
+++ js/app.js
@@ -4,28 +4,35 @@ var init = function() {
 	console.log('Hello World!');
 };
 
+var onCommandClick = function() {
+	var intervalName = [ "PRIME", "SEKUNDE", "TERZ", "QUARTE", "QUINTE",
+			"SEXTE", "SEPTIME", "OKTAVE" ];
+	var intervalQuestion = [ "c' nach c'", "c' nach d'", "c' nach e'",
+			"c' nach f'", "c' nach g'", "c' nach a'", "c' nach h'",
+			"c' nach c''" ];
 
+	for ( var i = 0; i < intervalQuestion.length; i++) {
+		console.log(intervalQuestion[i]);
+		console.log(intervalName[i]);
 
-var intervalName = ["PRIME", "SEKUNDE", "TERZ", "QUARTE", "QUINTE", "SEXTE", "SEPTIME", "OKTAVE"];
-var intervalQuestion = ["c' nach c'", "c' nach d'", "c' nach e'", "c' nach f'", "c' nach g'", "c' nach a'", "c' nach h'", "c' nach c''"];
+		var userAnswer = prompt(
+				"Nenne das Intervall von " + intervalQuestion[i]).toUpperCase();
 
-
-
-for (var i = 0; i < intervalQuestion.length; i++) {
-	console.log(intervalQuestion[i]);
-	console.log(intervalName[i]);
-	
-	var userAnswer = prompt("Nenne das Intervall von " + intervalQuestion[i]).toUpperCase();
-	
-	
-	if (userAnswer === intervalName[i]) {
-		alert("Das ist richtig.");
+		if (userAnswer === intervalName[i]) {
+			alert("Das ist richtig.");
+		} else {
+			alert("Das ist falsch.");
+		}
 	}
-	else {
-		alert("Das ist falsch.");
-	}
-}
-	
-	
-	
-	
+};
+
+var whenReady = function() {
+	$(".commands").click(onCommandClick);
+	$('.accordion').accordion({
+		collapsible : true,
+		heightStyle: 'fill'
+	});
+};
+
+$(document).ready(whenReady);
+
